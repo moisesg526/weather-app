bf9c84b4608a9d1edc780f0f5f3452117f743ca0
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-09-27 23:45:18 -0400

    Fixed an error that was not returning the current weather from the api

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let key = "S5993HFYTHTPJ65Q3QMB3NEEJ";
+let locationOfWeather = prompt("Location");
+
 fetch(
-  `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=${key}`,
+  `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationOfWeather}?key=${key}`,
   { mode: "cors" }
 )
-  .then(function (res) {
-    console.log(res.json());
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.currentConditions.temp);
   })
-  
+  .catch((err) => {
+    console.error(err);
+  });
