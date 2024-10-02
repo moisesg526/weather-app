ccbcd567cc99c4b5c108e0e3e6ca3b2c5ec0438e
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-10-02 00:06:58 -0400

    Displayed the weather temperature inside of the weather block

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let key = "S5993HFYTHTPJ65Q3QMB3NEEJ";
 let locationOfWeather = prompt("Location");
 
+let weatherBlock = document.querySelector(".weather-block");
+let h1 = document.createElement("h1");
+weatherBlock.appendChild(h1);
+
+let temp;
+
 fetch(
   `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationOfWeather}?key=${key}`,
   { mode: "cors" }
 )
   .then((res) => res.json())
   .then((data) => {
-    console.log(data.currentConditions.temp);
+    temp = data.currentConditions.temp;
+    h1.textContent = temp;
   })
   .catch((err) => {
     console.error(err);
