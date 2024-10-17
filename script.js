b70e0060e2f80bbaef51270a1b88d12a5c4c4518
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-10-16 22:47:50 -0400

    Created the function displayWeatherData to display the weather data

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ let weatherBlock = document.querySelector(".weather-block");
 let h1 = document.createElement("h1");
 weatherBlock.appendChild(h1);
 
-let temp;
+function WeatherData(temp, address, description) {
+  this.temp = temp;
+  this.address = address;
+  this.description = description;
+}
 
 fetch(
   `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationOfWeather}?key=${key}`,
@@ -13,8 +17,20 @@ fetch(
 )
   .then((res) => res.json())
   .then((data) => {
-    temp = data.currentConditions.temp;
-    h1.textContent = temp;
+    const displayInfo = new WeatherData(
+      data.currentConditions.temp,
+      data.address,
+      data.currentConditions.conditions
+    );
+
+    console.log(displayInfo);
+    function displayWeatherData() {
+      h1.innerHTML = `${displayInfo.address} <br>
+        ${displayInfo.temp} <br>
+        ${displayInfo.description}
+    `;
+    }
+    displayWeatherData();
   })
   .catch((err) => {
     console.error(err);
